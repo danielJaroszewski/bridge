3aa9ca675341315100cccfe98d9c8aa911c5699b
Daniel Jaroszewski <jaroszewskid1@gmail.com>
2022-05-17 00:28:11 +0200

    fields of OurComponent

--- include/OurComponent.hpp
+++ include/OurComponent.hpp
@@ -2,5 +2,14 @@
 
 class OurComponent
 {
+    private:
+    float xCoordinate;
+    float yCoordinate;
+    float length;
+    float width;
+    float density;
+
+    public:
+    OurComponent(float xCoordinate_, float yCoordinate_, float length_, float width_, float density_);
     
 };
