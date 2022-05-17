5549d5ede73eec90c7092d08b62c0a9f458aed4a
Daniel Jaroszewski <01149835@pw.edu.pl>
2022-05-17 22:24:15 +0200

    some methods for OurComponent

---
+++ src/OurComponent.cpp
@@ -0,0 +1,18 @@
+#include "../include/OurComponent.hpp"
+
+OurComponent::OurComponent(float xCoordinate_, float yCoordinate_, float length_, float width_, float density_)
+:xCoordinate(xCoordinate_), yCoordinate(yCoordinate_), length(length_), width(width_), density(density_)
+{}
+
+
+void OurComponent::createBodyDefinition()
+{
+    bodyDef.type = b2_dynamicBody;
+    bodyDef.position.Set(this->xCoordinate, this->yCoordinate);
+    // ...
+}
+
+b2BodyDef OurComponent::getBodyDef()
+{
+    return this->bodyDef;
+}
