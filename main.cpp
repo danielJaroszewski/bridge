671a7a25bd90746ef5b95f3aba7b85d4d028db08
Daniel Jaroszewski <01149835@pw.edu.pl>
2022-05-17 22:25:30 +0200

    main method for testing

--- main.cpp
+++ main.cpp
@@ -1,7 +1,12 @@
+#include "include/OurWorld.hpp"
+#include <iostream>
 
+using std::cout;
 
 int main(int argc, char const *argv[])
 {
+    OurWorld world = OurWorld(-10.0f);
+    world.initialize();
+    cout<<"\ndupa\n";
     return 0;
-    //checking
 }
