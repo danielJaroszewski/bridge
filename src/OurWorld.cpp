c357a5434a76420e5dab4df7288cb2ef9dffb395
Daniel Jaroszewski <01149835@pw.edu.pl>
2022-05-17 22:24:36 +0200

    initialize method

--- src/OurWorld.cpp
+++ src/OurWorld.cpp
@@ -1,4 +1,4 @@
-#include "OurWorld.hpp"
+#include "../include/OurWorld.hpp"
 
 OurWorld::OurWorld(float gravityFactor_)
 :gravityFactor(gravityFactor_)
@@ -9,4 +9,16 @@ void OurWorld::addComponent(float xCoordinate_, float yCoordinate_, float length
 {
     OurComponent newComponent = OurComponent(xCoordinate_, yCoordinate_, length_, width_, density_);
     components.push_back(newComponent);
+}
+
+void OurWorld::initialize()
+{
+    b2Vec2 gravity(0.0f, -10.0f);
+    b2World world(gravity);
+    b2BodyDef def;
+    for(auto i:components)
+    {
+        def = i.getBodyDef();
+        world.CreateBody(&def);
+    }
 }
