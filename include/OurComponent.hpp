b20f1f5717d97f0452f88d7d351e8cac90e7c3c3
Daniel Jaroszewski <01149835@pw.edu.pl>
2022-05-17 22:23:29 +0200

    Added body definition to OurComponent

--- include/OurComponent.hpp
+++ include/OurComponent.hpp
@@ -1,15 +1,19 @@
-
+#include "../box2d/include/box2d/box2d.h"
 
 class OurComponent
 {
-    private:
+private:
     float xCoordinate;
     float yCoordinate;
     float length;
     float width;
     float density;
+    b2BodyDef bodyDef;
 
-    public:
+public:
     OurComponent(float xCoordinate_, float yCoordinate_, float length_, float width_, float density_);
-    
+
+    void createBodyDefinition();
+    b2BodyDef getBodyDef();
+
 };
