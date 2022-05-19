0770fc7d7f101b046c2b1d925e3ac28e58e557ff
Daniel Jaroszewski <01149835@pw.edu.pl>
2022-05-20 00:55:34 +0200

    adding box2dBodies to world

--- include/OurWorld.hpp
+++ include/OurWorld.hpp
@@ -12,14 +12,17 @@ private:
     float gravityFactor = -10.0f; // to initialize gravity in box2d, default value
     vector<OurComponent> components;
     b2Vec2 gravity = b2Vec2(0.0f, gravityFactor);
-    b2World world = b2World(gravity);
+    b2World world =b2World(gravity);
+    vector<b2Body*> b2Bodies;
 public:
     OurWorld(float gravityFactor_);
 
     // xCoordinate and yCoordinate - coordinates of center of component
-    void addComponent(OurComponent component_);
+    void addComponent(OurComponent& component_);
 
     void addJoint(OurComponent bodyA, OurComponent bodyB);
     void initialize(); //making box2d world and box2d bodies
+    void assignFixtures();
+    void destroyB2World();
 
 };
