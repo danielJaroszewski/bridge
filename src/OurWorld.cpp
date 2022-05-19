2da6ecaacffd8cbfacbfa99029e8dcc06e3ce092
Daniel Jaroszewski <01149835@pw.edu.pl>
2022-05-20 00:56:45 +0200

    initializing box2dBodies and assigning fixtures

--- src/OurWorld.cpp
+++ src/OurWorld.cpp
@@ -1,11 +1,12 @@
 #include "../include/OurWorld.hpp"
+#include <iostream>
 
 OurWorld::OurWorld(float gravityFactor_)
 :gravityFactor(gravityFactor_)
 {    
 }
 
-void OurWorld::addComponent(OurComponent component_)
+void OurWorld::addComponent(OurComponent& component_)
 {
     components.push_back(component_);
 }
@@ -14,10 +15,40 @@ void OurWorld::initialize()
 {
     gravity = b2Vec2(0.0f, gravityFactor);
     world.SetGravity(gravity);
-    b2BodyDef def;
+    b2BodyDef bodyDef;
     for(auto i:components)
     {
-        def = i.getBodyDef();
-        world.CreateBody(&def);
+        bodyDef = i.getBodyDef();
+        try
+        {
+            b2Bodies.push_back(world.CreateBody(&bodyDef));
+        }
+        catch(const std::exception& e)
+        {
+            std::cerr << e.what() << '\n';
+        }
+        
     }
+}
+
+void OurWorld::assignFixtures()
+{
+    b2FixtureDef fixDef;
+    for(int i=0; i<components.size(); i++)
+    {
+        try
+        {
+            fixDef = components[i].getFixtureDefinition();
+            b2Bodies[i]->CreateFixture(&fixDef);
+        }
+        catch(const std::exception& e)
+        {
+            std::cerr << e.what() << '\n';
+        }     
+    }
+}
+
+void OurWorld::destroyB2World()
+{
+
 }
