1d32dd399ec59849ecd315379391ecd45672bc95
Daniel Jaroszewski <01149835@pw.edu.pl>
2022-05-21 00:00:36 +0200

    Joint

---
+++ src/OurJoint.cpp
@@ -0,0 +1,32 @@
+#include "../include/OurJoint.hpp"
+
+
+void OurJoint::initializeDefinition(b2Body* bodyA_, b2Body* bodyB_ , b2Vec2 aBodyAnchor_, b2Vec2 bBodyAnchor_)
+{
+    distJointDef.Initialize(bodyA_, bodyB_, aBodyAnchor_, bBodyAnchor_);
+    distJointDef.collideConnected = true;
+}
+
+
+void OurJoint::setLinearStiffness(float freq_, float dampingRatio_)
+{
+    b2LinearStiffness(distJointDef.stiffness, distJointDef.damping, freq_, dampingRatio_, distJointDef.bodyA, distJointDef.bodyB);
+}
+
+void OurJoint::setIndexOfBodies(int bodyAIndex, int bodyBIndex)
+{
+    indexOfBodyA = bodyAIndex;
+    indexOfBodyB = bodyBIndex;
+}
+
+int OurJoint::getBodyAIndex()
+{
+    return indexOfBodyA;
+}
+
+
+int OurJoint::getBodyBIndex()
+{
+    return indexOfBodyB;
+}
+
