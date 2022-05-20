5df19916b09daf8c16ae516691784af9059ee715
Daniel Jaroszewski <01149835@pw.edu.pl>
2022-05-21 00:00:05 +0200

    obsługa OurJoint w klasie OurWorld

--- include/OurWorld.hpp
+++ include/OurWorld.hpp
@@ -1,6 +1,7 @@
 #pragma once
 
 #include "./OurComponent.hpp"
+#include "./OurJoint.hpp"
 #include "box2d/box2d.h"
 #include <vector>
 
@@ -11,18 +12,18 @@ class OurWorld
 private:
     float gravityFactor = -10.0f; // to initialize gravity in box2d, default value
     vector<OurComponent> components;
+    vector<OurJoint> joints;
     b2Vec2 gravity = b2Vec2(0.0f, gravityFactor);
     b2World world =b2World(gravity);
-    vector<b2Body*> b2Bodies;
+    vector<b2Body*> b2Bodies; //spróbować przenieść do klasy OurComponent - błędy pamięci
+    vector<b2Joint*> b2Joints;
 public:
     OurWorld(float gravityFactor_);
-
-    // xCoordinate and yCoordinate - coordinates of center of component
     void addComponent(OurComponent& component_);
-
-    void addJoint(OurComponent bodyA, OurComponent bodyB);
-    void initialize(); //making box2d world and box2d bodies
+    void addJoint(OurJoint& joint_);
+    void initializeComponents(); //making box2d world and box2d bodies
+    void initializeJoints();
     void assignFixtures();
-    void destroyB2Bodies();
+    void destroyB2BodiesAndJoints();
 
 };
