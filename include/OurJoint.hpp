150554327902ccabcdd82afb9e878569de2de4a3
Daniel Jaroszewski <01149835@pw.edu.pl>
2022-05-20 23:59:36 +0200

    created OurJoint

---
+++ include/OurJoint.hpp
@@ -0,0 +1,20 @@
+#pragma once
+#include "box2d/box2d.h"
+
+class OurJoint // distance joint ?
+{
+private:
+   b2DistanceJointDef distJointDef;
+   int indexOfBodyA, indexOfBodyB;
+  
+public:
+    
+
+    void initializeDefinition(b2Body* bodyA_, b2Body* bodyB_ , b2Vec2 aBodyAnchor_, b2Vec2 bBodyAnchor_); //called from world
+    void setLinearStiffness(float freq_, float dampingRatio_);//must be called after initializeDefinition
+    void setIndexOfBodies(int bodyAIndex, int bodyBIndex);
+    int getBodyAIndex();
+    int getBodyBIndex();
+    
+    
+};
