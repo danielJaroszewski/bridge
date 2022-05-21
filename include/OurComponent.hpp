3779bdf5d2ae02dbf35b24db8accc5b483ef7ffe
Daniel Jaroszewski <01149835@pw.edu.pl>
2022-05-21 15:27:28 +0200

    Proper methods for returning body definitions

--- include/OurComponent.hpp
+++ include/OurComponent.hpp
@@ -15,10 +15,11 @@ private:
     b2PolygonShape dynamicBox;
 
 public:
+    b2Body* dynBody;
     OurComponent(float xCoordinate_, float yCoordinate_, float length_, float width_, float density_);
     void createBodyDefinition();
     void createBodyFixtureDefinition();
-    b2BodyDef getBodyDef();
-    b2FixtureDef getFixtureDefinition();
+    const b2BodyDef* getBodyDef();
+    const  b2FixtureDef* getFixtureDefinition();
     float getXCoordinate();
 };
