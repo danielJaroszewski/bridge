26f13e3816358af3c1020601e7fc8168990e4968
Daniel Jaroszewski <01149835@pw.edu.pl>
2022-05-31 01:12:00 +0200

    some implementation of simulation

--- include/Simulation.hpp
+++ include/Simulation.hpp
@@ -6,10 +6,12 @@ class Simulation
 {
     private:
     OurWorld simulationWorld;
-
+    float timeStep;
+    int32 velocityIterations, positionIterations;
 
     public:
     void setSimWorld(OurWorld& simWorld_); //to be called after creating all the components and joints - passing world to simulation
+    void setSimParams(float timeStep_=(1.0f/60.0f),int32 velocityIterations_=8, int32 positionIterations_ =3); //must be called before starting updating
     void update();
 
 };
---
+++ src/Simulation.cpp
@@ -0,0 +1,18 @@
+#include "../include/Simulation.hpp"
+
+void Simulation::setSimWorld(OurWorld& simWorld_)
+{
+    this->simulationWorld = simWorld_;
+}
+
+void Simulation::setSimParams(float timeStep_,int32 velocityIterations_, int32 positionIterations_)
+{
+    this->timeStep = timeStep_;
+    this->velocityIterations = velocityIterations_;
+    this->positionIterations = positionIterations_;
+}
+
+void Simulation::update()
+{
+    
+}
