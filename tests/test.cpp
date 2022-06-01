b6450cdc70fc7fdc557609cfa40124f4f7b4431e
Daniel Jaroszewski <01149835@pw.edu.pl>
2022-06-02 00:04:33 +0200

    deleted redundant tests

--- tests/test.cpp
+++ tests/test.cpp
@@ -1,24 +1,6 @@
 #include "catch2/catch_test_macros.hpp"
 #include "../include/OurWorld.hpp"
 
-TEST_CASE("some")
-{
-    OurWorld world = OurWorld();
-    OurComponent comp1 = OurComponent(0.0f, 1.0f, 4.0f, 5.0f, 10.0f);
-    REQUIRE(comp1.getBodyDef()->position.x == 0.0f);
-}
-
-TEST_CASE("second")
-{
-    OurWorld world = OurWorld();
-    OurComponent comp1 = OurComponent(0.0f, 1.0f, 4.0f, 5.0f, 10.0f);
-    comp1.createBodyDefinition();
-    comp1.createBodyFixtureDefinition();
-    world.addComponent(comp1);
-
-    OurComponent someComp = world.getComponent(0);
-    REQUIRE(someComp.getBodyDef()->position.x == 0.0f);
-}
 
 TEST_CASE("World init")
 {
@@ -38,7 +20,7 @@ TEST_CASE("World init")
 
     world.initializeWorld();
 
-    REQUIRE(comp1.getBodyDef()->position.x == 0.0f);
+    REQUIRE(comp1.getXCoordinate() == 0.0f);
 }
 
 TEST_CASE("positions")
