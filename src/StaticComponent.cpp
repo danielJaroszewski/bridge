98ef7069e15133d52d0621a6cc94a4b9a373f73a
Daniel Jaroszewski <01149835@pw.edu.pl>
2022-06-02 21:07:15 +0200

    Further methods - analog to OurComponent class. Maybe too similar

--- src/StaticComponent.cpp
+++ src/StaticComponent.cpp
@@ -10,3 +10,19 @@ void StaticComponent::createBodyDefinition()
     bodyDef.type = b2_staticBody;
     bodyDef.position.Set(xCoordinate, yCoordinate);
 }
+
+void StaticComponent::createBodyFixtureDefinition()
+{
+    staticBox.SetAsBox(0.25f, 0.25f); // constant and the same size
+    fixtureDef.shape = &staticBox;
+}
+
+const b2BodyDef* StaticComponent::getBodyDef()
+{
+    return &bodyDef;
+}
+
+const b2FixtureDef* StaticComponent::getFixtureDefinition()
+{
+    return &fixtureDef;
+}
