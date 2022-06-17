d4f2e80a66b53acb3f4e3f7c5c24e8f688a824ba
Daniel Jaroszewski <jaroszewskid1@gmail.com>
2022-06-17 17:05:49 +0200

    Example of creating car in main

--- main.cpp
+++ main.cpp
@@ -7,16 +7,6 @@ int main(int argc, char const *argv[])
 {
     OurWorld world = OurWorld();
 
-    OurComponent leftBase = OurComponent(1.0f, 1.0f, 50.0f, 10.0f, 50.0f);
-    leftBase.createBodyDefinition();
-    leftBase.createBodyFixtureDefinition();
-    world.addComponent(leftBase);
-
-    OurComponent rightBase = OurComponent(150.0f, 1.0f, 50.0f, 10.0f, 50.0f);
-    rightBase.createBodyDefinition();
-    rightBase.createBodyFixtureDefinition();
-    world.addComponent(rightBase);
-
     OurComponent comp1 = OurComponent(50.0f, 25.0f, 10.0f, 1.0f, 50.0f);
     comp1.createBodyDefinition();
     comp1.createBodyFixtureDefinition();
@@ -30,6 +20,10 @@ int main(int argc, char const *argv[])
     OurJoint joint1 = OurJoint();
     joint1.setIndexOfBodies(0,1);
     world.addJoint(joint1);
+
+    Car car = Car();
+    car.setUpCar();
+    world.addCar(car);
 
     world.initializeWorld();
 
