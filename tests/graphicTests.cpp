2ea5138a9f0eeaae1a471e4aa8762b08bb8b4cd5
Daniel Jaroszewski <jaroszewskid1@gmail.com>
2022-06-20 19:03:00 +0200

    New test file

---
+++ tests/graphicTests.cpp
@@ -0,0 +1,7 @@
+#include "catch2/catch_test_macros.hpp"
+#include "../include/OurWorld.hpp"
+#include <iostream>
+#include "../include/Graphics/GraphicsWorld.hpp"
+#include "../include/GUI/GUIWorld.hpp"
+#include "../include/GameScene.hpp"
+#include "../include/Graphics/BeamVisuals.hpp"
