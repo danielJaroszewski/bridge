c4ce9601fbeccf6d9b0ffbc6ce5e43e3ddb50738
Michal Bojke <01153433@pw.edu.pl>
2022-06-23 12:29:16 +0200

    Added CarVisuals test

--- tests/graphicTests.cpp
+++ tests/graphicTests.cpp
@@ -1,10 +1,11 @@
+#include <iostream>
 #include "catch2/catch_test_macros.hpp"
 #include "../include/OurWorld.hpp"
-#include <iostream>
-#include "../include/Graphics/GraphicsWorld.hpp"
-#include "../include/GUI/GUIWorld.hpp"
 #include "../include/GameScene.hpp"
+#include "../include/GUI/GUIWorld.hpp"
+#include "../include/Graphics/GraphicsWorld.hpp"
 #include "../include/Graphics/BeamVisuals.hpp"
+#include "../include/Graphics/CarVisuals.hpp"
 
 
 TEST_CASE("single dynamic component")
@@ -165,5 +166,55 @@ TEST_CASE("Complex test with 2 components")
         graphics.present();
     }
     
+
+}
+
+TEST_CASE("Car test")
+{
+
+        OurWorld world;
+
+    most::GraphicsWorld graphics;
+    auto& guiWorld = graphics.addDrawable(std::make_unique<most::GUI::World>());
+
+    bool shouldRun = true;
+    graphics.addEventCallback([&shouldRun](const sf::Event& e)
+    {
+        if (e.type == sf::Event::Closed)
+        {
+            shouldRun = false;
+        }
+    });
+
+    Car car;
+    car.setUpCar();
+    world.addCar(car);
+    graphics.addDrawable(std::make_unique<most::CarVisuals>(car));
+
+    world.initializeWorld();
+    #ifdef GRA
+        auto& gameScene = graphics.addDrawable(std::make_unique<most::GameScene>());
+        auto level = std::make_unique<most::Level>();
+
+
+        gameScene.setLevel(std::move(level));
+    #endif
+
+    long long accumulatedTime = 0;
+    constexpr long long fixedUpdateTime = 16666;
+    // Main loop.
+    while (shouldRun)
+    {
+        graphics.processEvents();
+        graphics.update();
+        accumulatedTime += graphics.getDeltaTime().asMicroseconds();
+        while (accumulatedTime >= fixedUpdateTime)
+        {
+            accumulatedTime -= fixedUpdateTime;
+            world.update();
+            graphics.physicsUpdate();
+        }
+        graphics.present();
+    }
 
 }
