e54113e26ee1a7414fcbd26977ec2ce0f66ebca0
Daniel Jaroszewski <jaroszewskid1@gmail.com>
2022-06-24 16:21:17 +0200

    Changing type of joint

--- src/OurJoint.cpp
+++ src/OurJoint.cpp
@@ -1,10 +1,11 @@
 #include "../include/OurJoint.hpp"
 
 
-void OurJoint::initializeDefinition(b2Body* bodyA_, b2Body* bodyB_ , b2Vec2 aBodyAnchor_, b2Vec2 bBodyAnchor_)
+void OurJoint::initializeDefinition(b2Body* bodyA_, b2Body* bodyB_ , b2Vec2 anchorPoint)
 {
-    distJointDef.Initialize(bodyA_, bodyB_, aBodyAnchor_, bBodyAnchor_);
-    distJointDef.collideConnected = true;
+    revJointDef.Initialize(bodyA_, bodyB_, anchorPoint);
+    revJointDef.collideConnected = false;
+
 }
 
 void OurJoint::setIndexOfBodies(int bodyAIndex, int bodyBIndex)
@@ -23,34 +24,15 @@ int OurJoint::getBodyBIndex()
     return indexOfBodyB;
 }
 
-const b2DistanceJointDef* OurJoint::getDistJointDef()
+const b2RevoluteJointDef* OurJoint::getReVJointDef()
 {
-    return &distJointDef;
+    return &revJointDef;
 }
 
-void OurJoint::setLeftBodyStatic()
-{
-    isLeftBStatic = true;
-}
-
-void OurJoint::setRightBodyStatic()
-{
-    isRightBStatic = true;
-}
-
-bool OurJoint::isLeftBodyStatic()
-{
-    return isLeftBStatic;
-}
-
-bool OurJoint::isRightBodyStatic()
-{
-    return isRightBStatic;
-}
 
 float OurJoint::getAbsActingForce()
 {
-    b2Vec2 force = distanceJoint->GetReactionForce(1/TIME_STEP);
+    b2Vec2 force = revoluteJoint->GetReactionForce(1/TIME_STEP);
     float modulus = sqrt(pow(force.x, 2) + pow(force.y, 2));
     return modulus;
 }
