f43e6f4afe9b08e77fe320a397c452feca1c1309
Daniel Jaroszewski <jaroszewskid1@gmail.com>
2022-06-26 19:37:34 +0200

    Some test

--- tests/graphicTests.cpp
+++ tests/graphicTests.cpp
@@ -209,6 +209,113 @@ TEST_CASE("with ledges")
         }
     });
 
+    Ledge ledge1 = Ledge(-10.0f, -0.5f);
+    ledge1.setUpLedge();
+    world.addLedge(ledge1);
+    graphics.addDrawable(std::make_unique<most::BeamVisuals>(ledge1));
+
+    Ledge ledge2 = Ledge(20.0f, -0.5f);
+    ledge2.setUpLedge();
+    world.addLedge(ledge2); 
+    graphics.addDrawable(std::make_unique<most::BeamVisuals>(ledge2));   
+
+    b2Vec2 lb = ledge1.getRightEdgeCoordinates();
+    b2Vec2 rb = ledge2.getLeftEdgeCoordiantes();
+
+    StaticComponent leftBase = StaticComponent(0.0f, 0.0f);
+    leftBase.setUpStaticComponent();
+    world.addStaticComponent(leftBase);
+    graphics.addDrawable(std::make_unique<most::BeamVisuals>(leftBase));
+
+
+    StaticComponent rightBase = StaticComponent(10.0f, 0.0f);
+    rightBase.setUpStaticComponent();
+    world.addStaticComponent(rightBase);
+    graphics.addDrawable(std::make_unique<most::BeamVisuals>(rightBase));
+
+
+    JoiningPoint jp1 = JoiningPoint(0.0f, 0.0f);
+    jp1.setUpJoiningPoint();
+    world.addJoiningPoint(jp1);
+
+
+    JoiningPoint jp2 = JoiningPoint(5.0f, 0.0f);
+    jp2.setUpJoiningPoint();
+    world.addJoiningPoint(jp2);
+
+    JoiningPoint jp3 = JoiningPoint(10.0f, 0.0f);
+    jp3.setUpJoiningPoint();
+    world.addJoiningPoint(jp3);
+
+
+    OurComponent comp1 = OurComponent(2.5f, 0.0f, 5.5f, 0.5f, 0.0f);
+    comp1.setUpComponent();
+    world.addComponent(comp1);
+    graphics.addDrawable(std::make_unique<most::BeamVisuals>(comp1));
+
+    OurComponent comp2 = OurComponent(7.5f, 0.0f, 5.5f, 0.5f, 0.0f);
+    comp2.setUpComponent();
+    world.addComponent(comp2);
+    graphics.addDrawable(std::make_unique<most::BeamVisuals>(comp2));
+
+
+    OurJoint j1;
+    j1.setIndexOfBodies(leftBase.getCompIndex(), jp1.getIndex());
+    j1.setBBodyJoiningPoint();
+    world.addJoint(j1);
+
+    OurJoint j2;
+    j2.setIndexOfBodies(jp1.getIndex(), comp1.getCompIndex());
+    j2.setABodyJoiningPoint();
+    world.addJoint(j2);
+
+    OurJoint j3;
+    j3.setIndexOfBodies(comp1.getCompIndex(), jp2.getIndex());
+    j3.setBBodyJoiningPoint();
+    world.addJoint(j3);
+
+    OurJoint j4;
+    j4.setIndexOfBodies(jp2.getIndex(), comp2.getCompIndex());
+    j4.setABodyJoiningPoint();
+    world.addJoint(j4);
+
+    OurJoint j5;
+    j5.setIndexOfBodies(comp2.getCompIndex(), jp3.getIndex());
+    j5.setBBodyJoiningPoint();
+    world.addJoint(j5);
+
+    OurJoint j6;
+    j6.setIndexOfBodies(jp3.getIndex(), rightBase.getCompIndex());
+    j6.setBBodyJoiningPoint();
+    world.addJoint(j6);
+
+
+    world.initializeWorld();
+
+    #ifdef GRA
+        auto& gameScene = graphics.addDrawable(std::make_unique<most::GameScene>());
+        auto level = std::make_unique<most::Level>();
+
+
+        gameScene.setLevel(std::move(level));
+    #endif
+    long long accumulatedTime = 0;
+    constexpr long long fixedUpdateTime = 16666;
+    // Main loop.
+    while (shouldRun)
+    {
+        graphics.processEvents();
+        graphics.update();
+        accumulatedTime += graphics.getDeltaTime().asMicroseconds();
+        while (accumulatedTime >= fixedUpdateTime)
+        {
+            accumulatedTime -= fixedUpdateTime;
+            world.update();
+            graphics.physicsUpdate();
+        }
+        graphics.present();
+       
+    }
     
 
 }
